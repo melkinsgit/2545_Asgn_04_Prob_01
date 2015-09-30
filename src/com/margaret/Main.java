b9e3886667cfe6757f9c22a025668491db28f8d8
melkinsgit <margaret.elkins@gmail.com>
2015-09-30 14:14:05 -0500

    Testing a commit

--- src/com/margaret/Main.java
+++ src/com/margaret/Main.java
@@ -4,10 +4,12 @@ public class Main {
 
     public static void main(String[] args) {
 
+        // create 3 new lakeRuns with one time each
         lakeRuns CalRuns = new lakeRuns ("Calhoun", 45.15);
         lakeRuns HarRuns = new lakeRuns ("Harriet", 49.34);
         lakeRuns ComRuns = new lakeRuns ("Como", 32.11);
 
+        // add run times to lakeRuns
         CalRuns.addTime(43.32);
         HarRuns.addTime(44.43);
         HarRuns.addTime(46.22);
